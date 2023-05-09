6f50a1d8d3135aa1a78961d18c2e314e8842904f
Miguel <miguel.viloria.247@gmail.com>
2023-05-09 12:31:02 -0400

    Endpoint-findOnePublication

---
+++ src/controller/PublicationController.ts
@@ -0,0 +1,24 @@
+import { AppDataSource } from '../data-source'
+import { NextFunction, Request, Response } from "express"
+import { Publication } from '../entity/Publication'
+
+export class PublicationController {
+
+    private publicationRepository = AppDataSource.getRepository(Publication)
+
+    async one(request: Request, response: Response, next: NextFunction){
+
+        const slug = request.params.slug
+
+        const publicaction =  await this.publicationRepository.findOne({
+            where: { slug }
+        })
+
+        if(!publicaction){
+            return "no se consiguió la publicación"
+        }
+
+        return publicaction
+
+    }
+}
