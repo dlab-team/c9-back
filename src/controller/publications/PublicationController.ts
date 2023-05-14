e6d7024facc7c8f615123fcd3c22a78f36b8dd45
Sebastian Vidal Aedo <sebastian@intercode.cl>
2023-05-14 13:17:46 -0400

    add comments

--- src/controller/publications/PublicationController.ts
+++ src/controller/publications/PublicationController.ts
@@ -6,6 +6,13 @@ import { asDTO, asDTOs } from './PublicationDTO';
 export class PublicationController {
   private publicationRepository = AppDataSource.getRepository(Publication);
 
+  /**
+   * Crea y guarda una nueva publicación en la base de datos.
+   * @param request - La solicitud HTTP que contiene los datos de la publicación.
+   * @param response - La respuesta HTTP que se enviará al cliente.
+   * @param next - La función que se llamará después de que se complete la operación.
+   * @returns La publicación creada.
+   */
   async save(request: Request, response: Response, next: NextFunction) {
     const { slug, name, initialContent, finalContent } = request.body;
     const publication = this.publicationRepository.create({
@@ -18,31 +25,46 @@ export class PublicationController {
     return results;
   }
 
+  /**
+   *
+   * @param request - La solicitud HTTP que contiene el slug de la publicación.
+   * @param response - La respuesta HTTP que se enviará al cliente.
+   * @param next - La función que se llamará después de que se complete la operación.
+   * @returns - La publicación con el slug especificado.
+   */
   async one(request: Request, response: Response, next: NextFunction) {
     const slug = request.params.slug;
     const publication = await this.publicationRepository.findOne({
       where: { slug },
       relations: {
         user: true,
-        questions: true
+        questions: true,
       },
       select: {
         user: {
           name: true,
         },
-      }
+      },
     });
     if (!publication) {
       return 'No se consiguió la publicación';
     }
-    return asDTO(publication);
+    const publicationDTO = asDTO(publication);
+    return publicationDTO;
   }
 
+  /**
+   * Obtiene todas las publicaciones de la base de datos.
+   * @param request - La solicitud HTTP que se está procesando.
+   * @param response - La respuesta HTTP que se enviará al cliente.
+   * @param next - La función que se llamará después de que se complete la operación.
+   * @returns Un arreglo de objetos DTO que representan las publicaciones.
+   */
   async all(request: Request, response: Response, next: NextFunction) {
     const publications = await this.publicationRepository.find({
       relations: {
         user: true,
-        questions: true
+        questions: true,
       },
       select: {
         user: {
@@ -51,12 +73,19 @@ export class PublicationController {
         questions: {
           question: true,
           answer: true,
-        }
-      }
+        },
+      },
     });
     return asDTOs(publications);
   }
 
+  /**
+   * Elimina una publicación de la base de datos.
+   * @param request - La solicitud HTTP que se está procesando.
+   * @param response - La respuesta HTTP que se enviará al cliente.
+   * @param next - La función que se llamará después de que se complete la operación.
+   * @returns Un mensaje que indica si la publicación se eliminó correctamente o no.
+   */
   async remove(request: Request, response: Response, next: NextFunction) {
     const slug = request.params.slug;
     const publicationToRemove = await this.publicationRepository.findOne({
@@ -69,6 +98,13 @@ export class PublicationController {
     return 'La Publicación se ha borrado correctamente';
   }
 
+  /**
+   * Actualiza una publicación existente en la base de datos.
+   * @param request - La solicitud HTTP que se está procesando.
+   * @param response - La respuesta HTTP que se enviará al cliente.
+   * @param next - La función que se llamará después de que se complete la operación.
+   * @returns La publicación actualizada.
+   */
   async update(request: Request, response: Response, next: NextFunction) {
     const slug = request.params.slug;
     const publication = await this.publicationRepository.findOne({
--- src/controller/publications/PublicationDTO.ts
+++ src/controller/publications/PublicationDTO.ts
@@ -1,25 +1,58 @@
-export function asDTO(response: any): { publication: any; } {
-   const publication: { [key: string]: any; } = {
-      id: response.id,
-      name: response.name,
-      slug: response.slug,
-      initialContent: response.initialContent,
-      finalContent: response.finalContent,
-      images: response.images.map((image: string) => ({ url: image })),
-      publicationDate: "2023/04/18", // TODO: Add publication date to publication
-      category: response.category,
-      region: "Metropolitana", // TODO: Add region to publication
-      city: "Santiago", // TODO: Add city to publication
-      author: response.user.name,
-      questions: response.questions.map((question: { question: string, answer: string; }) => ({
-         question: question.question,
-         answer: question.answer
-      }))
-   };
-   return { publication };
+/**
+ * Convierte una publicación de la base de datos en un objeto DTO.
+ * @param response - La publicación de la base de datos.
+ * @returns Un objeto DTO que representa la publicación.
+ */
+export function asDTO(response: any): { publication: any } {
+  const {
+    id,
+    name,
+    slug,
+    initialContent,
+    finalContent,
+    images,
+    category,
+    createdAt,
+    user,
+    questions,
+  } = response;
+
+  const date = new Date(createdAt);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1; // Los meses en JavaScript comienzan en 0, por lo que debemos sumar 1
+  const day = date.getDate();
+  const publicationDate = `${year}/${month.toString().padStart(2, '0')}/${day
+    .toString()
+    .padStart(2, '0')}`;
+
+  const publication = {
+    id,
+    name,
+    slug,
+    initialContent,
+    finalContent,
+    images: images.map((image: string) => ({ url: image })),
+    publicationDate,
+    category,
+    region: 'Metropolitana', // TODO: Add region to publication
+    city: 'Santiago', // TODO: Add city to publication
+    author: user.name,
+    questions: questions.map(
+      (question: { question: string; answer: string }) => ({
+        question: question.question,
+        answer: question.answer,
+      })
+    ),
+  };
+  return { publication };
 }
 
-export function asDTOs(response: any[]): { publications: any[]; } {
-   const publications = response.map((response) => asDTO(response).publication);
-   return { publications };
+/**
+ * Convierte un arreglo de publicaciones de la base de datos en un arreglo de objetos DTO.
+ * @param response - El arreglo de publicaciones de la base de datos.
+ * @returns Un objeto DTO que representa el arreglo de publicaciones.
+ */
+export function asDTOs(response: any[]): { publications: any[] } {
+  const publications = response.map((response) => asDTO(response).publication);
+  return { publications };
 }
