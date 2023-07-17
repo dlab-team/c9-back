14b2bd0206bc76ebf6f659b66bb538e20fe06ed7
Alfredo Villegas <alfreddovillegas@gmail.com>
2023-07-17 00:48:44 -0400

    add: finalContent_EN field  in publication entity/controllers

--- src/entity/Publication.ts
+++ src/entity/Publication.ts
@@ -28,6 +28,9 @@ export class Publication {
 
   @Column({ type: 'text' })
   finalContent: string;
+
+  @Column({ type: 'text', nullable: true, default: '' })
+  finalContent_EN: string;
   
   @Column({ type: 'timestamp', nullable: true })
   fecha_publicacion: Date;
