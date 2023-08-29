92477d470c9b4ad3abdc39f90684ea8aa5869617
Alfredo Villegas <alfreddovillegas@gmail.com>
2023-08-29 18:06:40 -0400

    add: chat implementation

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,9 @@ import userRouter from "./controller/users/UserRoutes";
 import cityRouter from './controller/cities/CityRoutes';
 import regionRouter from "./controller/regions/RegionRoutes";
 import authorRouter from "./controller/authors/AuthorRoutes";
+import chatRouter from "./controller/chats/ChatRoutes";
 
-export const Routes = [publicationRouter, questionRouter, userRouter, cityRouter, regionRouter, authorRouter ]
+export const Routes = [publicationRouter, questionRouter, userRouter, cityRouter, regionRouter, authorRouter , chatRouter]
 
 export default function registerRouter (app: any) {
     Routes.map((router) => {app.use(router)})
